71ee60ae05ea9acd75a079e6b712f72c7f987474
Araon <soumik.ghosh1998@gmail.com>
2024-01-11 00:05:31 +0530

    Refactor URL processing and improve expiration handling
    Consolidate URL processing into a postData object, enhance expiration
    handling logic, and update redirects for expired URLs. Improve error
    handling and overall reliability of URL shortening functionality.

--- controller/shortController.js
+++ controller/shortController.js
@@ -16,27 +16,32 @@ const getAllShortURLs = async (req, res) => {
 const createShortURL = async (req, res) => {
   try {
 
-    const { url } = req.body;
+    const postData = req.body;
 
-    const existingShortURL = await shortdb.findOne({ longUrl: url });
+    const existingShortURL = await shortdb.findOne({ longUrl: postData.url });
 
     if (existingShortURL) {
       const response = {
         key: existingShortURL.code,
         long_url: existingShortURL.longUrl,
         short_url: existingShortURL.shortUrl,
+        expire: existingShortURL.expire
       };
       res.status(200).send(response);
       return;
     }
 
-    const key = uuv4().substring(0, 6); // Generating a short key
+    // Generating a short key
+    const key = uuv4().substring(0, 6);
+
+    // Generating a shortUrl using the host and the protocol it's hosted on.
     const shortURL = `${req.protocol}://${req.get('host')}/short/${key}`;
 
     const newShortURL = new shortdb({
       code: key,
-      longUrl: url,
+      longUrl: postData.url,
       shortUrl: shortURL,
+      expire: postData.expire
     });
 
     const result = await newShortURL.save();
@@ -45,6 +50,7 @@ const createShortURL = async (req, res) => {
       key: result.code,
       long_url: result.longUrl,
       short_url: result.shortUrl,
+      expire: result.expire
     };
 
     res.status(201).send(response);
@@ -59,16 +65,26 @@ const redirectToLongURL = async (req, res) => {
     const shortURL = await shortdb.findOne({ code, isActive: true });
 
     if (shortURL) {
-      shortURL.clicks = (shortURL.clicks || 0) + 1;
-      await shortURL.save();
-      res.redirect(302, shortURL.longUrl);
+      // Check if expiration date is less than current date
+      if (shortURL.expire && new Date(shortURL.expire) < new Date()) {
+        // Expired, show 404
+        res.status(404).send("Short URL has expired");
+      } else {
+        // Not expired, update clicks and redirect
+        shortURL.clicks = (shortURL.clicks || 0) + 1;
+        await shortURL.save();
+        res.redirect(302, shortURL.longUrl);
+      }
     } else {
+      // Short URL not found
       res.status(404).send("Short URL not found");
     }
   } catch (err) {
+    // Handle other errors
     res.status(500).send(err.message);
   }
 };
+
 
 module.exports = {
   getAllShortURLs,
