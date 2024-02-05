38d3f2a4dc842c6f04c0e6959d2a52abb14098a4
Araon <soumik.ghosh1998@gmail.com>
2024-02-05 22:43:39 +0530

    Refactor gitignore, logging, and docker setup
    - Remove redundant healthcheck from docker-compose.yml to streamline
    setup

--- models/dbConnect.js
+++ models/dbConnect.js
@@ -6,7 +6,7 @@ require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 
 const sequelize = new Sequelize(process.env.MYSQL_DB_NAME, process.env.MYSQL_DB_USER, process.env.MYSQL_DB_PASSWORD, {
-  host: process.env.MYSQL_DB_HOST,
+  // host: process.env.MYSQL_DB_HOST,
   port: 3306,
   dialect: 'mysql',
 });
