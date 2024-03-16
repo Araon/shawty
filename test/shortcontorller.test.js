9f35a3c87eb750dbdc7d838826d7ab6855a94446
Soumik <soumik.ghosh1998@gmail.com>
2024-03-17 00:32:45 +0530

    Update shortcontorller.test.js
    updated from browser

--- test/shortcontorller.test.js
+++ test/shortcontorller.test.js
@@ -1,36 +1,40 @@
 const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../app'); // app.js
+
+chai.use(chaiHttp);
+
 const expect = chai.expect;
 
+describe('URL Shortener', () => {
 
+  it('should shorten a URL', async () => {
+    const res = await chai.request(server)
+      .post('/short')
+      .send({
+        url: 'https://www.google.com/'
+      });
+    
+    expect(res).to.have.status(200);
+    expect(res.body).to.have.property('short_url');
+  });
 
-describe('Test cases test', () => {
-    it('test case sub description', () => {
-        actualVal = 10
-        expectedVal = 10
-        expect(actualVal).to.be.equal(expectedVal)
-    })
-})
+  it('should redirect to original URL', async () => {
+    const {short_url} = await chai.request(server)
+      .post('/short')
+      .send({
+        url: 'https://www.google.com/'  
+      });
 
-// describe('Checking the health route', () => {
-//     it('should retun a 200 status code', (done) => {
-//         chai.request(app)
-//         .get('/ping')
-//         .end((err, res) => {
-//             res.should.have.status(200);
-//             expect(res.body.message).to.be.equal('OK')
-//             done();
-//         });
-        
-//     })
-// })
+    const res = await chai.request(server).get(short_url);
+    
+    expect(res).to.redirectTo('https://www.google.com/');
+  });
 
-// describe('Checking database short code', () => {
-//     it('should retun a 200 status code', (done) => {
-//         chai.request(app)
-//         .get('/short')
-//         .end((err, res) => {
-//             res.should.have.status(200);
-//             done();
-//         });
-//     })
-// })
+  it('should return 404 for invalid short URL', async () => {
+    const res = await chai.request(server).get('/short/abc123');
+
+    expect(res).to.have.status(404);
+  });
+
+});
